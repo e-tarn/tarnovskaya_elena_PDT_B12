e19579c5a576fa5a6176c6f257400b2c3cf08607
e-tarn <e.tarnovskaya@hotmail.com>
2020-02-18 10:27:35 +0200

    Created module Addressbook, added tests for Group and Contact creation

---
+++ addressbook-web-tests/src/test/java/com/stqa/addressbook/ConcactData.java
@@ -0,0 +1,43 @@
+package com.stqa.addressbook;
+
+public class ConcactData {
+  private final String fname;
+  private final String lname;
+  private final String address;
+  private final String homePhone;
+  private final String mobPhone;
+  private final String email;
+
+  public ConcactData(String fname, String lname, String address, String homePhone, String mobPhone, String email) {
+    this.fname = fname;
+    this.lname = lname;
+    this.address = address;
+    this.homePhone = homePhone;
+    this.mobPhone = mobPhone;
+    this.email = email;
+  }
+
+  public String getFname() {
+    return fname;
+  }
+
+  public String getLname() {
+    return lname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomePhone() {
+    return homePhone;
+  }
+
+  public String getMobPhone() {
+    return mobPhone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/com/stqa/addressbook/GroupCreationTests.java
@@ -0,0 +1,48 @@
+package com.stqa.addressbook;
+
+
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+
+public class GroupCreationTests extends  TestBase{
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
+    goToGroupPage();
+    initGroupCreation();
+    fillGroupCreationForm(new GroupData("name", "g_header", "g_footer"));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupCreationForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void goToGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+
+
+}
+
+
