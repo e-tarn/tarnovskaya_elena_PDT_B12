92ea363e098f343d1b2fe418f1de6b0974a16c5d
e-tarn <e.tarnovskaya@hotmail.com>
2020-02-25 15:19:45 +0200

    Changes in class ApplicationManager m. init() - added browser selection

--- addressbook-web-tests/src/test/java/com/stqa/addressbook/manager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/com/stqa/addressbook/manager/ApplicationManager.java
@@ -1,10 +1,10 @@
 package com.stqa.addressbook.manager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,16 +14,29 @@ public class ApplicationManager{
   ContactHelper contact;
   SessionHelper session;
   NavigationHelper nav;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
 
   public void init() {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    session = new SessionHelper(wd);
-    nav = new NavigationHelper(wd);
-    group = new GroupHelper(wd);
-    contact = new ContactHelper(wd);
-    session.login("admin", "secret");
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+
+      wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      wd.get("http://localhost/addressbook/");
+      session = new SessionHelper(wd);
+      nav = new NavigationHelper(wd);
+      group = new GroupHelper(wd);
+      contact = new ContactHelper(wd);
+      session.login("admin", "secret");
+    }
   }
 
   public void stop() {
--- addressbook-web-tests/src/test/java/com/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/stqa/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package com.stqa.addressbook.tests;
 
 import com.stqa.addressbook.manager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class TestBase {
 
-  protected ApplicationManager app = new ApplicationManager();
+  protected ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
