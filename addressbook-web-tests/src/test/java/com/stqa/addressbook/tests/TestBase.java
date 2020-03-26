1f288a4f155304123fb46b888549898c3b450b7b
e-tarn <e.tarnovskaya@hotmail.com>
2020-03-26 17:18:03 +0200

    addeg logBack

--- addressbook-web-tests/src/test/java/com/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/stqa/addressbook/tests/TestBase.java
@@ -2,14 +2,22 @@ package com.stqa.addressbook.tests;
 
 import com.stqa.addressbook.manager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
   protected static ApplicationManager app =
           new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
+Logger logger = LoggerFactory.getLogger(TestBase.class);
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
     app.init();
@@ -19,6 +27,16 @@ public class TestBase {
   public void tearDown() throws Exception {
     app.stop();
 
+  }
+  @BeforeMethod
+  public void startLogger(Method m, Object[] p){
+    logger.info("Start test "+ m.getName()
+            + " with parameters: " + Arrays.asList(p));
+  }
+
+  @AfterMethod
+  public void stopLogger(Method m){
+    logger.info("Stop test "+ m.getName() );
   }
 
 }
