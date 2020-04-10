1e918521323f85c083ef6c696689af15d4c8776d
e-tarn <e.tarnovskaya@hotmail.com>
2020-04-11 00:40:53 +0300

    added remote.properties

--- addressbook-web-tests/src/test/java/com/stqa/addressbook/manager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/com/stqa/addressbook/manager/ApplicationManager.java
@@ -1,17 +1,21 @@
 package com.stqa.addressbook.manager;
 
 import com.stqa.addressbook.model.ContactData;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -36,15 +40,21 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
+if("".equals(properties.getProperty("selenium.server"))){
+  if (browser.equals(BrowserType.FIREFOX)) {
+    wd = new FirefoxDriver();
+  } else if (browser.equals(BrowserType.CHROME)) {
+    wd = new ChromeDriver();
+  } else if (browser.equals(BrowserType.IE)) {
+    wd = new InternetExplorerDriver();
+  }
+} else{
+  DesiredCapabilities capabilities = new DesiredCapabilities();
+  capabilities.setBrowserName(browser);
+  wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+}
     dbHelper = new DbHelper();
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
+
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wait = new WebDriverWait(wd, 25);
     wd.get(properties.getProperty("web.baseUrl","http://localhost/addressbook/"));
